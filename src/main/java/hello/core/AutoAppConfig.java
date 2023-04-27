eeab144691d3c39e7cf5d19e62c943e1e97ed4c1
OkDH <ocko112@gmail.com>
2023-04-27 22:45:40 +0900

    다양한 의존관계 주입 방법 및 옵션처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,13 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         // 기존의 만든 AppConfig를 스캔하지 않도록 설정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 }
+
+
+
+
